ea99dd05231f9f5962aeb3dd9e7d98a5f19f9cb9
NikolayK <nikolay.kim@codeimmersive.com>
2019-09-30 14:59:21 -0400

    adds solution to almost all exercises

--- main.js
+++ main.js
@@ -1,27 +1,48 @@
-let iGetTheJoke;
-let havingFun;
-let learning;
-let killingIt;
+let iGetTheJoke = true;
+let havingFun = true;
+let learning = true;
+let killingIt = true;
 
 
-function returnFalse() {
-
+function returnFalse(str) {
+  str = false; 
+  return str;
 }
 
 function isOpposite(condition) {
-
+  if (condition === false){
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 function both(condition1, condition2) {
-
+  if (condition1 && condition2) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 function either(condition1, condition2) {
-
+  if (condition1 || condition2) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 function neither(condition1, condition2) {
-
+  if (condition1 || condition2) {
+    return false;
+  }
+  else {
+    return true;
+  }
 }
 
 function itsComplicated(condition1, condition2, condition3) {
